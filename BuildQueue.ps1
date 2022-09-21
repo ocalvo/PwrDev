b2e30d47d4e14011f87a993fcf870d839c6f5b87
Oscar Calvo <ocalvo@microsoft.com>
2022-09-21 15:28:38 -0600

    Add Start-PullRequestBuildAction

--- BuildQueue.ps1
+++ BuildQueue.ps1
@@ -42,3 +42,56 @@ function global:Test-PullRequestUpToDate
 
 Export-ModuleMember -Function Test-PullRequestUpToDate
 
+function global:Get-PullRequestChecks
+{
+
+    if ($null -eq $pullRequest)
+    {
+        $pullRequest = Get-PullRequest | Select -First 1
+    }
+
+    $checks = az repos pr policy list --id ($pullRequest.pullRequestId) | ConvertFrom-Json
+    return $checks
+}
+
+Export-ModuleMember -Function Get-PullRequestChecks
+
+
+function global:Get-PullRequestBuildActions
+{
+    param(
+      [Parameter(Mandatory=$true)]$PullRequestChecks)
+
+    $buildActions = $PullRequestChecks | Where-Object { $null -ne $_.context.buildDefinitionId }
+
+    return $buildActions
+}
+
+Export-ModuleMember -Function Get-PullRequestBuildActions
+
+function global:Start-PullRequestBuildAction
+{
+    param(
+        [Parameter(Mandatory=$true)]$PullRequest,
+        [Parameter(Mandatory=$true)]$Checks
+    )
+
+    $action = $Checks |% { az repos pr policy queue --evaluation-id $_.evaluationId --id $PullRequest.pullRequestId | ConvertFrom-Json }
+    return $action
+}
+
+Export-ModuleMember -Function Start-PullRequestBuildAction
+
+function global:Get-PullRequestBuildActionStatus
+{
+    param(
+        [Parameter(Mandatory=$true)]$BuildAction
+    )
+
+    $NewBuildAction = $BuildAction |% { az pipelines runs show --id $_.context.buildId | ConvertFrom-Json }
+
+    return $NewBuildAction
+}
+
+Export-ModuleMember -Function Get-PullRequestBuildActionStatus
+
