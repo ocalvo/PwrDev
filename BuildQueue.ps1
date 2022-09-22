1620fedccfb3e628d8bd5ad2d0bcbbeb374314eb
Oscar Calvo <ocalvo@microsoft.com>
2022-09-22 13:56:11 -0600

    Add support for Job

--- BuildQueue.ps1
+++ BuildQueue.ps1
@@ -13,9 +13,13 @@ if ($null -eq (get-command git -ErrorAction Ignore))
 
 function global:Get-PullRequest()
 {
-    param()
+    param(
+        [switch]$Verbose
+    )
 
     $currentBranch = (git branch --show-current)
+    Write-Verbose "Current branch is $currentBranch" -Verbose:$verbose
+
     if ($null -eq $currentBranch)
     {
         Write-Error "No git branch detected"
@@ -24,9 +28,11 @@ function global:Get-PullRequest()
 
     $branchFullName = ('refs/heads/'+$currentBranch)
     $currentPR = az repos pr list | ConvertFrom-Json | Where-Object -Property sourceRefName -EQ $branchFullName
-
-    $lastCommit = (git log --format="%H" -n 1)
-
+    if ($null -eq $currentPR)
+    {
+        Write-Error "Cannot get pull request"
+        return;
+    }
     return $currentPR
 }
 
@@ -35,22 +41,32 @@ Export-ModuleMember -Function Get-PullRequest
 function global:Test-PullRequestUpToDate
 {
     param(
-      [Parameter(Mandatory=$true)]$pullRequest)
+        [switch]$Verbose,
+        [Parameter(Mandatory=$true)]$pullRequest = (Get-PullRequest -Verbose $verbose | Select-Object -First 1)
+    )
 
-    return (git log --format="%H" -n 1) -eq $pullRequest.lastMergeSourceCommit.commitId
+    $lastCommitId = $pullRequest.lastMergeSourceCommit.commitId
+    Write-Verbose "Last pushed commit $lastCommitId" -Verbose:$verbose
+
+    $gitLastCommit = (git log --format="%H" -n 1)
+    Write-Verbose "Last local commit $gitLastCommit" -Verbose:$verbose
+
+    return $gitLastCommit -eq $lastCommitId
 }
 
 Export-ModuleMember -Function Test-PullRequestUpToDate
 
 function global:Get-PullRequestChecks
 {
+    param(
+        [switch]$Verbose,
+        $PullRequest = (Get-PullRequest -Verbose:$verbose | Select -First 1)
+    )
 
-    if ($null -eq $pullRequest)
-    {
-        $pullRequest = Get-PullRequest | Select -First 1
-    }
+    $prID = $pullRequest.pullRequestId
+    Write-Verbose "Getting policy checks for PR $prID" -Verbose:$verbose
 
-    $checks = az repos pr policy list --id ($pullRequest.pullRequestId) | ConvertFrom-Json
+    $checks = az repos pr policy list --id ($prID) | ConvertFrom-Json | Where-Object { $null -ne $_.context.buildDefinitionId }
     return $checks
 }
 
@@ -60,7 +76,11 @@ Export-ModuleMember -Function Get-PullRequestChecks
 function global:Get-PullRequestBuildActions
 {
     param(
+      [switch]$verbose,
       [Parameter(Mandatory=$true)]$PullRequestChecks)
+
+    $c = $PullRequestChecks.Count
+    Write-Verbose "Getting build actions that have a build definition id from $c checks" -Verbose:$verbose
 
     $buildActions = $PullRequestChecks | Where-Object { $null -ne $_.context.buildDefinitionId }
 
@@ -69,29 +89,72 @@ function global:Get-PullRequestBuildActions
 
 Export-ModuleMember -Function Get-PullRequestBuildActions
 
-function global:Start-PullRequestBuildAction
+function global:Get-PullRequestBuildStatus
 {
     param(
-        [Parameter(Mandatory=$true)]$PullRequest,
-        [Parameter(Mandatory=$true)]$Checks
+        [switch]$verbose,
+        [Parameter(Mandatory=$true)]$Build
     )
 
-    $action = $Checks |% { az repos pr policy queue --evaluation-id $_.evaluationId --id $PullRequest.pullRequestId | ConvertFrom-Json }
-    return $action
+    $b = $Build | Select-Object -First 1;
+    $bId = $b.context.buildId
+    Write-Verbose "Getting build status for build $bId" -Verbose:$verbose
+
+    $BuildStatus = $Build |% { az pipelines runs show --id $_.context.buildId | ConvertFrom-Json }
+
+    $status = $BuildStatus.status
+    Write-Verbose "Getting build status is $status" -Verbose:$verbose
+
+    return $BuildStatus
 }
 
-Export-ModuleMember -Function Start-PullRequestBuildAction
+Export-ModuleMember -Function Get-PullRequestBuildStatus
 
-function global:Get-PullRequestBuildActionStatus
+function global:Monitor-PullRequestBuildActions
 {
     param(
-        [Parameter(Mandatory=$true)]$BuildAction
+        [Parameter(Mandatory=$true)]$Build
     )
 
-    $NewBuildAction = $BuildAction |% { az pipelines runs show --id $_.context.buildId | ConvertFrom-Json }
-
-    return $NewBuildAction
+    $v = $false
+    if ($VerbosePreference -eq "Continue") { $v = $true }
+    do
+    {
+        $BuildStatus = Get-PullRequestBuildStatus $Build -Verbose $v
+        Sleep 1
+    }
+    while ($BuildStatus.status.Contains("completed"))
 }
 
-Export-ModuleMember -Function Get-PullRequestBuildActionStatus
+Export-ModuleMember -Function Monitor-PullRequestBuildActions
+
+
+function global:Start-PullRequestBuildActions
+{
+    param(
+        [switch]$verbose,
+        $PullRequest = (Get-PullRequest -Verbose $verbose | Select-Object -First 1),
+        $Checks = (Get-PullRequestChecks -Verbose $verbose -PullRequest $PullRequest)
+    )
+
+    $prId = $PullRequest.pullRequestId
+    Write-Verbose "Queueing new build for PR $prId" -Verbose:$verbose
+
+    $build = $Checks |% { az repos pr policy queue --evaluation-id $_.evaluationId --id $prId | ConvertFrom-Json }
+
+    $bId = $build.context.buildId
+    Write-Verbose "Build $bId started" -Verbose:$verbose
+
+    [scriptblock] $monitorFunc = {
+        Import-Module PwrDev
+        $v = $false
+        if ($args[1] -ne $null) { $v = $args[1] }
+        if ($v) { $VerbosePreference = "Continue" }
+        Monitor-PullRequestBuildActions -Build $args[0]
+    }
+    $v = $verbose
+    return Start-Job -ScriptBlock $monitorFunc -ArgumentList ($build,$v)
+}
+
+Export-ModuleMember -Function Start-PullRequestBuildActions
 
